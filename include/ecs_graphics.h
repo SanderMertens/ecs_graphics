e8c08b06aa85175a4a3b36a01dc0ec3456827262
Sander Mertens <sander.mertens8@gmail.com>
2019-03-16 22:54:52 -0700

    Initial commit

---
+++ include/ecs_graphics.h
@@ -0,0 +1,16 @@
+#ifndef ECS_GRAPHICS_H
+#define ECS_GRAPHICS_H
+
+/* This generated file contains includes for project dependencies */
+#include "bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
